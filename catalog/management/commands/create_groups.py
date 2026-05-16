bff9932269b7cacb4a600f93e54d6c7734d39dbd
Маргарита Буршева <mbursheva@mail.ru>
2026-05-16 18:33:47 +0300

    Update catalog/management/commands/create_groups.py

--- catalog/management/commands/create_groups.py
+++ catalog/management/commands/create_groups.py
@@ -1,6 +1,6 @@
+from django.contrib.auth.models import Group, Permission
 from django.contrib.contenttypes.models import ContentType
 from django.core.management.base import BaseCommand
-from django.contrib.auth.models import Group, Permission
 
 from catalog.models import Product
 
@@ -10,12 +10,14 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         # Создаём или получаем группу
-        group, created = Group.objects.get_or_create(name='Модератор продуктов')
+        group, created = Group.objects.get_or_create(name="Модератор продуктов")
 
         content_type = ContentType.objects.get_for_model(Product)
 
         # Получаем нужные права: кастомное can_unpublish_product и стандартное delete_product
-        perms = Permission.objects.filter(content_type=content_type, codename__in=['can_unpublish_product', 'delete_product'])
+        perms = Permission.objects.filter(
+            content_type=content_type, codename__in=["can_unpublish_product", "delete_product"]
+        )
 
         # Назначаем права группе
         group.permissions.set(perms)
